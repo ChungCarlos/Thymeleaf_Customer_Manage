165a4e6335d162bbbb26c8b2cea54374c275a7d1
Chung <Joschungcarlos95@gmail.com>
2023-06-02 17:02:43 +0700

    done TH customer

---
+++ src/main/java/com/codegym/service/CustomerService.java
@@ -0,0 +1,60 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerService implements ICustomerService {
+    private static final List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        customers.add(new Customer(1, "John", "john@codegym.vn", "Hanoi"));
+        customers.add(new Customer(2, "Bill", "bill@codegym.vn", "Danang"));
+        customers.add(new Customer(3, "Alex", "alex@codegym.vn", "Saigon"));
+        customers.add(new Customer(4, "Adam", "adam@codegym.vn", "Beijin"));
+        customers.add(new Customer(5, "Sophia", "sophia@codegym.vn", "Miami"));
+        customers.add(new Customer(6, "Rose", "rose@codegym.vn", "Newyork"));
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.add(customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        for (Customer customer : customers) {
+            if (customer.getId() == id) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        for (int i = 0; i < customers.size(); i++) {
+            if (customers.get(i).getId() == id) {
+                customers.set(i, customer);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        customers.removeIf(customer -> customer.getId() == id);
+    }
+}
